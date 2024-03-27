cf3f1c3f4bde23a5720e32d7d9dc9336b00bbac6
JoaoGabeee <136844527+JoaoGabeee@users.noreply.github.com>
2024-03-27 17:14:32 -0300

    Update Cliente.java

--- Exercicio01/src/Cliente.java
+++ Exercicio01/src/Cliente.java
@@ -45,4 +45,9 @@ public class Cliente {
     public void setTelefoneCliente(long telefoneCliente) {
         this.telefoneCliente = telefoneCliente;
     }
+
+    public void reservarQuarto(){
+
+    }
+    
 }
